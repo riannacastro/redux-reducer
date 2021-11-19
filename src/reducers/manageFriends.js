4ef2bce4c0be1de1c0665fc5e08ef4e78db73025
Rianna Castro <riannaxcastro@gmail.com>
2021-11-19 10:15:20 -1000

    commit tree ff5b4bcf2470dc9cf26c5e78f9923d3fcf2bc3f3

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+
+    switch (action.type) {
+        case "ADD_FRIEND":
+            return {...state, friends: [...state.friends, action.friend]}
+        case "REMOVE_FRIEND":
+            const remover = state.friends.findIndex(friend => friend.id === action.id)
+            return {...state, friends: [...state.friends.slice(0, remover),
+                ...state.friends.slice(remover + 1)]}
+            default: 
+                return state;
+    }
 }
